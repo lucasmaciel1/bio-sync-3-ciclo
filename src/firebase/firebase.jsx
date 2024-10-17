27a65e9e16cdc569fd1181465ffa040be2154d71
lucasmaciel1 <lucas.m.maciel@hotmail.com>
2024-10-17 11:02:56 -0300

    Versão MVP

---
+++ src/firebase/firebase.jsx
@@ -0,0 +1,27 @@
+// src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAa8atbXwwofrRgmHHXTcnvAiyVvMv2Oyo",
+    authDomain: "biosync-32e95.firebaseapp.com",
+    projectId: "biosync-32e95",
+    storageBucket: "biosync-32e95.appspot.com",
+    messagingSenderId: "873929644381",
+    appId: "1:873929644381:web:d4f9bf98d2d32103c9b0b4",
+    measurementId: "G-T1RVV2X796"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Initialize App Check
+initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LceH2IqAAAAACmtimxf5YJ9xFIMiHjN02rPp1Sj'),
+  isTokenAutoRefreshEnabled: true
+});
+
+// Initialize Firebase Authentication
+const auth = getAuth(app);
+
+export { auth };
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+    
+  ],
+  theme: {
+    colors:{
+      black:{
+        1: '#000000',
+      },
+      green:{
+        1: '#50E18A',
+      },
+      gray:{
+        1: '#D9D9D9',
+      },
+      white:{
+        1:'#FFFFFF',
+      },
+    },
+    extend: {
+      opacity:{
+        '80': '.80',
+        '95': '.95',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      backgroundImage: {
+        'back-header': "url('/public/back-header-map-V3.jpg')",
+        'back-ambiente-conscientizacao': "url('/public/back-cont.png')"
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
